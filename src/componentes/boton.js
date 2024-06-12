46a25538f0fa3e840a0c9e99ea5df6c10575fcda
JoseDev <josevelazquezolvera2@gmail.com>
2024-06-12 15:13:50 -0600

    primer commit

---
+++ src/componentes/boton.js
@@ -0,0 +1,30 @@
+import React from "react";
+import '../hojas-de-estilo/Boton.css'
+
+
+function Boton(props) {
+  
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
+  };
+
+  if (esOperador(props.children)) {
+    return (
+      <div 
+        className='boton-contenedor operador'
+        onClick={() => props.manejarClic(props.children)}>
+        {props.children}
+      </div>
+    );
+  } else {
+    return (
+      <div 
+        className='boton-contenedor'
+        onClick={() => props.manejarClic(props.children)}>
+        {props.children}
+      </div>
+    );
+  }
+}
+
+export default Boton;
